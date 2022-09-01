9ea51a14149b1e81134cfe23cbd8d889b9be86da
Alex <65793940+alexchan73@users.noreply.github.com>
2022-09-01 16:25:59 -0700

    Update metadata_extract.py

--- metadata_extract.py
+++ metadata_extract.py
@@ -1,49 +1,60 @@
+"""Implementing wtf i just did """
 #Main project
-
 import os
 import time
 import colorama
-from colorama import Fore, Back, Style
-from exiftool import ExifToolHelper
+from colorama import Back, Fore, Style #Text with color package
 
-#https://sylikc.github.io/pyexiftool/examples.html      pyexiftool api
+from exiftool import ExifToolHelper #main meta data extraction package
+#https://sylikc.github.io/pyexiftool/examples.html
+#have to install pyexiftool
+ 
 
 def exif_tool():
-    exif_input = input("Please enter the full directory of the file you want to extract from: ") #Examples can be "/Users" or "/yourname"
+    exif_input = input("Please enter the full directory of the file you want to extract from: ")
     with ExifToolHelper() as et:
         for d in et.get_metadata(str(exif_input)):
-            for k, v in d.items():       #k = key , v = value
-                print(f"Type: {k} = {v}")  #formatting as a dictionary
+            for k, v in d.items():
+                print( Fore.MAGENTA + str(f"Info {k} = {v}"))
 
-def list_directory(usr_input): 
-    for i in os.listdir(usr_input):
+def list_directory(path_input):
+    target_files = []
+    folder_list = []
+    misc_files = []
+    for i in os.listdir(path_input):
         if i.endswith(".jpg"):
-            my_list.append(i)
+            target_files.append(i)
         elif i.endswith(".pdf"):
-            my_list.append(i)
+            target_files.append(i)
         elif i.endswith(".txt"):
-            my_list.append(i)
+            target_files.append(i)
         elif i.endswith(".png"):
-            my_list.append(i)
-    print(Fore.BLUE + str(my_list))
- 
-def main():
-    print("".join(("=",)*40))
-    print("".join(("+",)*40))
-    print("".join(("=",)*40))
+            target_files.append(i)
+        elif i.startswith("."):
+            misc_files.append(i)
+        elif os.path.isdir(path_input):
+            folder_list.append(i)
+    print(Fore.RED + str(target_files))
+    print(Fore.YELLOW + str(folder_list))
+    print(Fore.BLUE + str(idk_files))
     
+
+def main():
+    print(FORE.GREEN + "".join(("=",)*80))
+    print(FORE.GREEN + "".join(("+",)*80))
+    print(FORE.GREEN + "".join(("=",)*80))
+    
+    #my_stack = []
     print("This is your current directory you are in: " + os.getcwd())
-    usr_input = input("Please enter directory to search for files:  ")
-    list_directory(usr_input)
+    path_input = input("Please enter directory to search for files:  ")
+    print("Red text is files that you can extract metadata from \nYellow text are folders/directories you can find other files \nBlue text is other files that starts with (.) ")
+    list_directory(path_input)
     while True: 
         cont_input = input("Do you wish to go further into the directory? (y/n): ")
         if(cont_input.upper() == "Y"):
             usr_input = input("Please enter directory to search for files:  ")
             list_directory(usr_input)
         elif(cont_input.upper() == "N"):
-            #list_directory(usr_input)
             break
     exif_tool()
-    
-
 main()
